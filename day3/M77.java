9389af7d27118331bd0ddc290522f4758fd8c068
yashoda-krishna <148547478+yashoda-krishna@users.noreply.github.com>
2024-07-08 14:37:09 +0530

    Update M77.java

--- day3/M77.java
+++ day3/M77.java
@@ -42,3 +42,55 @@ public class M77 {
 		return flag;
 	}
 }
+
+
+
+/*
+
+
+
+package strings_app2;
+
+import java.util.Scanner;
+
+public class M22 {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter first string");
+	String s1 = sc.next();
+	System.out.println("enter 2nd string");
+	String s2 = sc.next();
+	if(s1.length() != s2.length()) {
+		System.out.println(s1 + " and "+ s2 + "are not anagrams");
+		return;
+	}
+	String temp = s2;
+	boolean flag = true;
+	char c1;
+	int index;
+	for(int i = 0; i < s1.length(); i++) {
+		c1 = s1.charAt(i);
+		index = temp.indexOf(c1);
+		if(index != -1) {
+			temp = temp.substring(0, index) + temp.substring(index + 1, temp.length());
+		}
+		else {
+			flag = false;
+			break;
+		}
+	}
+	
+	if(temp.length() != 0) {
+		flag = false;
+	}
+	System.out.println(s1 +" and " + s2 +" are " + (flag ? "anagrams" : "not anagrams"));
+}
+}
+
+
+
+
+
+
+*/
+
